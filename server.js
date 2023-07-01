aa9ddc12b383e539ad46804b11106abf0e2782cb
Enrypase <pasettoenrico02@gmail.com>
2023-07-01 22:50:08 +0200

    fix: added ping route

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(verifyToken);
 // db connection
 connectDb();
 // routes
-app.use("/users", require("./routes/api/users"));
+app.use("/ping", (req, res) => res.status(200).json({ message: "Pong!" }));
+// app.use("/users", require("./routes/api/users"));
 app.use("/login", require("./routes/login"));
 // start express
 const PORT = process.env.PORT;
