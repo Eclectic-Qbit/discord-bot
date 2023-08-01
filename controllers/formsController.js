d89198e4a8f90ceed811ba06a00ff4c68bf6abec
Enrico <73340659+Enrypase@users.noreply.github.com>
2023-08-01 12:49:20 +0200

    debug: ""

--- controllers/formsController.js
+++ controllers/formsController.js
@@ -1,6 +1,6 @@
 async function insertFirstForm(req, res) {
   console.log("Inserting form");
-  console.log(req.query);
+  console.log(req.body);
   return res.sendStatus(200);
 }
 module.exports = { insertFirstForm };
